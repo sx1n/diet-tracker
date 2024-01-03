20ec9162f8b862ea8c74eebb1dcdb40f76d1271b
sx1n <samuellaurindodelima2004@gmail.com>
2024-01-03 16:56:06 -0300

    feat: gender register

--- app/src/main/java/com/sx1n/diettracker/data/viewModels/RegisterViewModel.kt
+++ app/src/main/java/com/sx1n/diettracker/data/viewModels/RegisterViewModel.kt
@@ -6,10 +6,15 @@ import androidx.lifecycle.ViewModel
 class RegisterViewModel: ViewModel() {
     val firstName = MutableLiveData<String>()
     val lastName = MutableLiveData<String>()
+    val sex = MutableLiveData<Char>()
 
     fun registerName(newFirstName: String, newLastName: String) {
         firstName.value = newFirstName
         lastName.value = newLastName
+    }
+
+    fun registerSex(newSex: Char) {
+        sex.value = newSex
     }
 
 }
--- app/src/main/java/com/sx1n/diettracker/ui/fragments/GenderRegistrationFragment.kt
+++ app/src/main/java/com/sx1n/diettracker/ui/fragments/GenderRegistrationFragment.kt
@@ -2,13 +2,16 @@ package com.sx1n.diettracker.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
 import com.sx1n.diettracker.R
+import com.sx1n.diettracker.data.viewModels.RegisterViewModel
 import com.sx1n.diettracker.databinding.FragmentGenderRegistrationBinding
-import com.sx1n.diettracker.databinding.FragmentPhysicalActivityRegistrationBinding
 import com.sx1n.diettracker.ui.listeners.OnStepChangedListener
 import com.sx1n.diettracker.utils.viewBinding
 
@@ -18,6 +21,8 @@ class GenderRegistrationFragment : Fragment(R.layout.fragment_gender_registratio
 
     private var stepChangedListener: OnStepChangedListener? = null
 
+    private val viewModel: RegisterViewModel by navGraphViewModels(R.id.register_navigation)
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnStepChangedListener) {
@@ -25,16 +30,46 @@ class GenderRegistrationFragment : Fragment(R.layout.fragment_gender_registratio
         } else {
             throw ClassCastException("$context must implement OnStepChangedListener")
         }
-
-        notifyStepChanged(2)
-    }
-
-    // Chame este método quando o passo for alterado no Fragment
-    private fun notifyStepChanged(step: Int) {
-        stepChangedListener?.onStepChanged(step)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        notifyStepChanged(2)
+
+        observe()
+
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_genderRegistrationFragment_to_heightRegistrationFragment)
+        }
+
+        binding.rgSex.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = binding.root.findViewById<RadioButton>(checkedId)
+            val sex: Char = if (radioButton == binding.rbMale) 'M' else 'F'
+
+            viewModel.registerSex(sex)
+        }
+    }
+
+    private fun notifyStepChanged(step: Int) {
+        stepChangedListener?.onStepChanged(step)
+    }
+
+    private fun enableButton() {
+        val btnNext = binding.btnNext
+        btnNext.isClickable = true
+        btnNext.isEnabled = true
+        btnNext.isLongClickable = true
+        btnNext.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+    }
+
+    private fun observe() {
+        viewModel.sex.observe(viewLifecycleOwner) { sex ->
+            if (sex == 'M') {
+                binding.rbMale.isChecked = true
+            } else {
+                binding.rbFemale.isChecked = true
+            }
+            enableButton()
+        }
     }
 }
